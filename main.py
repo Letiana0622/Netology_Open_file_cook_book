321c98833447dd77274b328b13fcbfd4b2e9f753
Letiana0622 <letia@mail.ru>
2022-08-04 00:09:24 +0300

    errors corrected via debugging in PC

--- main.py
+++ main.py
@@ -3,52 +3,50 @@
 # Задание1
 # Должен получится следующий словарь
 
+#advice - Задача 1 - вы забыли что есть строка разделитель, а значит надо делать “фейковое чтение” строки -
+# вам бы помогло использование отдладчика, вы бы увидели как и куда движется ваш код.
+# "for line in file_work:
+#             dish_name = <что-то тут делаем>
+#             counter = <как-то получили кол-во строк>
+#             list_of_ingridient = <временный список>
+#             for i in <наш счетчик counter использовать range>:
+#                 temp_dict = {} - временный словарь
+#                 ingridient = file_work.readline() - вот так перемещаемся по файлу
+#                 <заполняем словарь с ингридиетом>
+#                 <добавляем словарь в список>
+#             <записываем в словарь cook_dict наш рецепт>
+#             file_work.readline() - еще раз смещаетмся т.к. там пустая срока"
+
 def open_file():
     cook_book = {}
-    ingredients_list = []
+    # ingredients_list = []
     with open('recipes.txt','r') as f:
-       for line in f: #без цикла словарь строится для Омлет (считываются верные строки название и к-во).
-          # ?при добавлении данного 1го цикла код считывает вместо к-ва 2й строки строку последнего ингредиента/
-          # ?не могу разобраться уже неделю. На вопрос о помощи не получила конкретного ответа по данной ошибке
-          dish_name = f.readline().strip()
+       for line in f:
+          dish_name = line.strip()
+          #dish_name = f.readline().strip()
           ingredients_qty = int(f.readline().strip())
+          ingredients_list = []
           # print(dish_name)
           # print(ingredients_qty)
        # print(dish_name)
        # print(ingredients_qty)
           for ingredient_line in range(ingredients_qty):
-               ingredients_qty -= 1
-               ingredient_dict = {}
-               item1, item2, item3 = f.readline().split("|")
-               ingredient_dict['ingredient_name'] = item1.strip(' ')
-               ingredient_dict['quantity'] = item2.strip(' ')
-               ingredient_dict['measure'] = item3.strip(' \n')
-               ingredients_list.append(ingredient_dict)
-               cook_book[dish_name] = ingredients_list
-       #
-          print(cook_book)
+              ingredient_dict_temp = {}
+              item1, item2, item3 = f.readline().split("|")
+              ingredient_dict_temp['ingredient_name'] = item1.strip(' ')
+              ingredient_dict_temp['quantity'] = item2.strip(' ')
+              ingredient_dict_temp['measure'] = item3.strip(' \n')
+              ingredients_list.append(ingredient_dict_temp)
+          cook_book[dish_name] = ingredients_list
+          f.readline()
+       print(cook_book)
 open_file()
-
+print()
+print()
+print()
 # Задание2
 #кулинарная книга из задания 1
-cook_book2 = {
-  'Омлет': [
-    {'ingredient_name': 'Яйцо', 'quantity': 2, 'measure': 'шт.'},
-    {'ingredient_name': 'Молоко', 'quantity': 100, 'measure': 'мл'},
-    {'ingredient_name': 'Помидор', 'quantity': 2, 'measure': 'шт'}
-    ],
-  'Утка по-пекински': [
-    {'ingredient_name': 'Утка', 'quantity': 1, 'measure': 'шт'},
-    {'ingredient_name': 'Вода', 'quantity': 2, 'measure': 'л'},
-    {'ingredient_name': 'Мед', 'quantity': 3, 'measure': 'ст.л'},
-    {'ingredient_name': 'Соевый соус', 'quantity': 60, 'measure': 'мл'}
-    ],
-  'Запеченный картофель': [
-    {'ingredient_name': 'Картофель', 'quantity': 1, 'measure': 'кг'},
-    {'ingredient_name': 'Чеснок', 'quantity': 3, 'measure': 'зубч'},
-    {'ingredient_name': 'Сыр гауда', 'quantity': 100, 'measure': 'г'},
-    ]
-  }
+cook_book = {'Омлет': [{'ingredient_name': 'Яйцо', 'quantity': '2', 'measure': 'шт'}, {'ingredient_name': 'Молоко', 'quantity': '100', 'measure': 'мл'}, {'ingredient_name': 'Помидор', 'quantity': '2', 'measure': 'шт'}], 'Утка по-пекински': [{'ingredient_name': 'Утка', 'quantity': '1', 'measure': 'шт'}, {'ingredient_name': 'Вода', 'quantity': '2', 'measure': 'л'}, {'ingredient_name': 'Мед', 'quantity': '3', 'measure': 'ст.л'}, {'ingredient_name': 'Соевый соус', 'quantity': '60', 'measure': 'мл'}], 'Запеченный картофель': [{'ingredient_name': 'Картофель', 'quantity': '1', 'measure': 'кг'}, {'ingredient_name': 'Чеснок', 'quantity': '3', 'measure': 'зубч'}, {'ingredient_name': 'Сыр гауда', 'quantity': '100', 'measure': 'г'}], 'Фахитос': [{'ingredient_name': 'Говядина', 'quantity': '500', 'measure': 'г'}, {'ingredient_name': 'Перец сладкий', 'quantity': '1', 'measure': 'шт'}, {'ingredient_name': 'Лаваш', 'quantity': '2', 'measure': 'шт'}, {'ingredient_name': 'Винный уксус', 'quantity': '1', 'measure': 'ст.л'}, {'ingredient_name': 'Помидор', 'quantity': '2', 'measure': 'шт'}]}
  # Нужно написать функцию, которая на вход принимает список блюд из cook_book и количество персон
  # для кого мы будем готовить  get_shop_list_by_dishes(dishes, person_count)
  # На выходе мы должны получить словарь с названием ингредиентов и его количества для блюда.
@@ -65,26 +63,25 @@ cook_book2 = {
 
 def get_shop_list_by_dishes(dishes, person_count):
     shop_list_ingredients = {}
-    shop_list_qty = {}
-    for dish, ingredients_list in cook_book2.items():
+
+    for dish, ingredients_list in cook_book.items():
         if dish in dishes:
             for item in ingredients_list:
+                shop_list_qty_temp = {} #temporary dictionary for collection of ingredients measure and qty in loop
                 shop_item = item['ingredient_name']
                 shop_measure = item['measure']
-                shop_qty = item['quantity'] * person_count
-                shop_list_qty['measure'] = shop_measure
-                shop_list_qty['quantity'] = shop_qty
-                shop_list_ingredients[shop_item] = shop_list_qty
+                shop_qty = int(item['quantity']) * int(person_count)
+                shop_list_qty_temp['measure'] = shop_measure
+                shop_list_qty_temp['quantity'] = shop_qty
+                shop_list_ingredients[shop_item] = shop_list_qty_temp
     print(shop_list_ingredients)
-    #? не получается отразить корректное измерение и к-во,
-    #? код выводит везде данные последнего ингредиента последнего блюда (также не получается самостоятельно разобраться)
 get_shop_list_by_dishes(['Омлет','Запеченный картофель'],2)
-
+print()
+print()
 
 # Задание №3
 # В папке лежит некоторое количество файлов. Считайте, что их количество и имена вам заранее известны,
-# пример для выполнения домашней работы можно взять тут
-# Необходимо объединить их в один по следующим правилам:
+# пример для выполнения домашней работы можно взять тут. Необходимо объединить их в один по следующим правилам:
 # Содержимое исходных файлов в результирующем файле должно быть отсортировано по количеству строк в них
 # (то есть первым нужно записать файл с наименьшим количеством строк, а последним - с наибольшим)
 # Содержимое файла должно предваряться служебной информацией на 2-х строках: имя файла и количество строк в нем
@@ -105,9 +102,9 @@ get_shop_list_by_dishes(['Омлет','Запеченный картофель']
 
 def combine_files():
     import os, fnmatch
-    files = []
     files = fnmatch.filter(os.listdir('.'), '*_.txt')
     print(files)
+    print()
     files_combined = {}
     file_data = []
     for file in files:
@@ -119,23 +116,24 @@ def combine_files():
             file_data.append(data)
             files_combined[file] = file_data
             file_data = []
-    # print(files_combined)
+    print()
+    print(files_combined)
     files_sorted = sorted(files_combined.items(), key=lambda kv: (kv[1], kv[0]))
-    # print(files_combined)
-    print(files_sorted)
+    print()
+    files_sorted_ = list(files_sorted)
+    print(files_sorted_)
+    print()
 
     with open('result.txt', 'w') as f_:
-        for items in files_sorted:
-            f_.write(str(files_sorted[0][0]))
+        for item in files_sorted_:
+            f_.write(str(item[0]))
             f_.write('\n')
-            f_.write(str(files_sorted[0][1][0]))
+            f_.write(str(item[1][0]))
             f_.write('\n')
-            f_.write(str(files_sorted[0][1][1]))
+            f_.write(str(item[1][1]))
             f_.write('\n')
             f_.write('\n')
-    # ?не могу разобраться почему при записи в файл записывается только 1й элемент списка?
-    # f_.close()
-
+    f_.close()
 combine_files()
 
 
